541c94a25c38cfe080b90db6fa6e2fee477ff90b
PreetiRanjanPradhan <220301120003@cutm.ac.in>
2024-11-16 14:10:30 +0530

    application.properties configuration

--- src/main/java/com/project/domain/DomainBackendApplication.java
+++ src/main/java/com/project/domain/DomainBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DomainBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DomainBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DomainBackendApplication.class, args);
+    }
 
 }
