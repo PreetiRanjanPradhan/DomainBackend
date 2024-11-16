f7b0b776cf9e9aa50b5091555d1aa268bc8e519f
PreetiRanjanPradhan <220301120003@cutm.ac.in>
2024-11-16 14:03:23 +0530

    Initial commit

---
+++ src/main/java/com/project/domain/DomainBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainBackendApplication.class, args);
+	}
+
+}
