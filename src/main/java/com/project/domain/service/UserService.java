09b112634b820eea41023a48ea92524c3941f29b
Jagannath720 <jagannathdashjagaa@gmail.com>
2024-11-18 00:46:48 +0530

    commit

---
+++ src/main/java/com/project/domain/service/UserService.java
@@ -0,0 +1,21 @@
+package com.project.domain.service;
+
+import java.util.Set;
+
+import org.springframework.web.multipart.MultipartFile;
+
+ipackage com.project.domain.models.User;
+import com.example.ExamPortal.models.UserRole;
+
+public interface UserService {
+
+	//Creating User
+	public User createUser(User user,Set<UserRole>userRoles) throws Exception;
+	//get user by username
+	public User getUser(String username);
+	//delete user by id
+	public void deleteUser(Long userId);
+	// Get all user details
+    public Set<User> getAllUsers();
+	Set<User> getAllAdmins();
+}
